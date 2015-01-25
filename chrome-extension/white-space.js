473c6f87f2a14860a30fb355c3df8b20917b0841
Brad Barwick <bbarwick@precisiondev.net>
2015-01-25 01:23:39 -0800

    Initial commit.

---
+++ chrome-extension/white-space.js
@@ -0,0 +1,19 @@
+
+;(function(){
+
+	var search 		= location.search.length
+		? location.search.replace('?', '').split('&')
+		: []
+	;
+
+	var wsParam 	= 'w=1';
+	var wsIndex 	= search.indexOf(wsParam);
+
+	if (wsIndex == -1) {
+		search.push(wsParam);
+		location.href = location.pathname + '?' + search.join('&');
+	}
+
+	console.log('search:', search);
+	console.log('wsIndex:', wsIndex);
+})();
