27b8d05b1fe45799f4f6d32594448885a6748e6c
underblob <brad@bradbarwick.com>
2015-03-20 14:47:01 -0700

    Preserve location hash
    When a direct link to a comment is loaded the hash is getting lost.

--- chrome-extension/white-space.js
+++ chrome-extension/white-space.js
@@ -11,6 +11,6 @@
 
 	if (wsIndex == -1) {
 		search.push(wsParam);
-		location.href = location.pathname + '?' + search.join('&');
+		location.href = location.pathname + '?' + search.join('&') + location.hash;
 	}
 })();
